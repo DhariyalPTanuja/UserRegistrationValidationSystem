d218d4e631d6e814eceed4d9ac87eba961e381af
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-02 15:59:09 +0530

    apply last name validation pattern

--- UserDetail.java
+++ UserDetail.java
@@ -1,10 +1,16 @@
 package com.userregistration;
 
+import java.util.Scanner;
+
 public class UserDetail {
 	String firstname;
+	String lastname;
 
-	public UserDetail(String fName) {
-		this.firstname = fName;
+	Scanner scanobj = new Scanner(System.in);
+	public UserDetail(String firstname, String lastname) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
 	}
 
 	public String getFirstname() {
@@ -14,10 +20,43 @@ public class UserDetail {
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-
-	@Override
-	public String toString() {
-		return "UserDetail [firstname=" + firstname + "]";
+	public String getLastname() {
+		return lastname;
 	}
 
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	@Override
+	public String toString() {
+		return "UserDetail [firstname=" + firstname + ", lastname=" + lastname + "]";
+	}
+
+	public void validateUserInput() {
+		System.out.println("Enter the first name: ");
+		String fName = scanobj.next();
+		patternMatching(fName);
+		 System.out.println("Enter the Last name: ");
+			String lName = scanobj.next();
+			patternMatching(lName);
+	}
+	private void checkValid(boolean regexResult) {
+		if(regexResult == false) {
+			System.out.println("Please Enter a valid Input");
+			System.out.println("Again Enter a Input");
+			String Name = scanobj.next();
+			patternMatching(Name);
+		}
+		else 
+			System.out.println("Matches");
+		
+	}
+	private void patternMatching(String name) {
+		String regex = "^[A-Z]{1}[a-z]{3,}$";
+		boolean regexResult =name.matches(regex);
+		 checkValid(regexResult);
+
+		
+	}
 }
+
--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -1,36 +1,20 @@
 package com.userregistration;
 
-import java.util.Scanner;
 
 public class UserRegistrationMain {
+
+	static String fName;
+	static String lName;
 	public static void main(String[] args) {
 
 		System.out.println("Welcome in the User Registration Validation System!!!!");
 		
-		// UC1 As a User need to enter a valid First Name
+		// UC2 As a User need to enter a valid Last Name
 		//- First name starts with Cap and has minimum 3 characters
-		UserRegistrationMain user = new UserRegistrationMain();
-		user.validateUserInput();
+
+		UserDetail user1 = new UserDetail(fName, lName);
+		user1.validateUserInput();
 		
-		
-	}
-	private void validateUserInput() {
-		Scanner scanobj = new Scanner(System.in);
-
-		System.out.println("Enter the first name: ");
-		String fName = scanobj.next();
-
-		String regex = "^[A-Z]{1}[a-z]{3,}$";
-		boolean regexResult =fName.matches(regex);
-		 checkValid(regexResult);
-
-	}
-	private void checkValid(boolean regexResult) {
-		if(regexResult == false) {
-			System.out.println("Please Enter a valid Input");
-		}
-		else 
-			System.out.println("Matches");
 		
 	}
 }
